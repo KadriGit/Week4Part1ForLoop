cf01e342bd9e48314a07d7148815a6cd87f1b17a
KadriGit <kadri.viira@tktk.ee>
2024-10-29 21:38:00 +0200

    Finised code for part 1

--- Week4Part1ForLoop/Program.cs
+++ Week4Part1ForLoop/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿for (int i = 0; i < 10; i++)
+    Console.WriteLine($"{i+1}. I will not skip Zoom classes");
